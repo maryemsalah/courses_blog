ef2d3d489ddea2d5af4864956fa701b10f8eb614
maryem salah <mariemsalah303@gmail.com>
2024-02-20 19:12:55 +0200

    add file

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
-/*
-document.getElementById("userForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from submitting
-
-    // Get user input
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var gender = document.getElementById("gender").value;
-
-    // Create a JavaScript object with user information
-    var userData = {
-        "name": name,
-        "email": email,
-        "password":password,
-        "gender":gender
-    };
-
-    // Convert JavaScript object to JSON string
-    var jsonData = JSON.stringify(userData);
-
-    // Store JSON data in local storage
-    localStorage.setItem("userData", jsonData);
-
-    // Optional: Display a message to the user
-    alert("User data saved successfully!");
-
-    // You can now access the stored user data using localStorage.getItem("userData");
-});*/
+/*regestration form */
 document.addEventListener("DOMContentLoaded", function() {
-    // Add event listener inside this function
+    
     var userForm = document.getElementById("userForm");
     if (userForm) {
         userForm.addEventListener("submit", function(event) {
@@ -55,10 +27,10 @@ document.addEventListener("DOMContentLoaded", function() {
             localStorage.setItem("userData", jsonData);
 
           
-            // Optional: Display a message to the user
+         
             alert("User data saved successfully!");
           
-            // You can now access the stored user data using localStorage.getItem("userData");
+           
         });
     } else {
         console.error("Element with ID 'userForm' not found.");
